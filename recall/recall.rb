f7d849bd0b952371aaf0eed150bf0c05bcca61d8
Mike Northorp <mike.northorp@gmail.com>
2013-08-27 12:27:45 -0700

    Added editing, deleitiion and completion

--- recall/recall.rb
+++ recall/recall.rb
@@ -34,4 +34,41 @@ post '/' do
 	redirect '/'
 end
 
+# Edit notes
+get '/:id' do
+	@note = Note.get params[:id]
+	@title = "Edit note ##{params[:id]}"
+	erb :edit
+end
+
+# Route for our put request
+put '/:id' do
+	n = Note.get params[:id]
+	n.content = params[:content]
+	n.complete = params[:complete] ? 1 : 0
+	n.updated_at = Time.now
+	n.save
+	redirect '/'
+end
+
+get '/:id/delete' do
+	@note = Note.get params[:id]
+	@title = "Confirm deleition of note ##{params[:id]}"
+	erb :delete
+end
+
+delete '/:id' do
+	n = Note.get params[:id]
+	n.destroy
+	redirect '/'
+end
+
+get '/:id/complete' do
+	n = Note.get params[:id]
+	n.complete = n.complete ? 0 : 1 # flip it
+	n.updated_at = Time.now
+	n.save
+	n.redirect '/'
+end
+
 
